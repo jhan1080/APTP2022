63eda745544dd41db0ecefa8fa3e6a314b92cca4
JUNHAN <junhan@naver.com>
2022-06-01 23:39:34 +0900

    로그인 문제 해결 안됨

--- semifinal.py
+++ semifinal.py
@@ -73,19 +73,112 @@ def exitApp():
     else:
         pass
 
+#####################################################
+# 기본설정 크롬드라이버 자동다운
+chrome_ver = chromedriver_autoinstaller.get_chrome_version().split('.')[0]
+driver_path = f'./{chrome_ver}/chromedriver.exe'
+if os.path.exists(driver_path):
+    print(f"ChromeDriver is installed: {driver_path}")
+else:
+    print(f"install the ChromeDriver(ver: {chrome_ver})")
+    chromedriver_autoinstaller.install(True)
+
+
 
 #로그인이 완료될시 1을 리턴
 #이 함수 하나만 만들어주시면 됩니다!
 def loggedIn():
-    #####################################################
-    # 기본설정 크롬드라이버 자동다운
-    chrome_ver = chromedriver_autoinstaller.get_chrome_version().split('.')[0]
-    driver_path = f'./{chrome_ver}/chromedriver.exe'
-    if os.path.exists(driver_path):
-        print(f"ChromeDriver is installed: {driver_path}")
-    else:
-        print(f"install the ChromeDriver(ver: {chrome_ver})")
-        chromedriver_autoinstaller.install(True)
+    # 런어스 홈페이지 접속
+    driver = webdriver.Chrome(driver_path)
+    url = "https://www.learnus.org/"
+    driver.implicitly_wait(15)
+    driver.get(url)
+    time.sleep(1)
+
+    # 로그인창 들어가기
+    driver.find_element(By.CSS_SELECTOR,
+                        "#page-header > div.main-header.page-util > div > div.usermenu > ul > li > a.btn.btn-sm.btn-loginout").click()
+    time.sleep(1)
+
+    # 아이디 입력창
+    id = driver.find_element(By.CSS_SELECTOR, "#ssoLoginForm > div > div:nth-child(1) > input:nth-child(3)")
+    id.click()
+    id.send_keys(ent1.get())
+    time.sleep(1)
+
+    # 비밀번호 입력창
+    pw = driver.find_element(By.CSS_SELECTOR, "#ssoLoginForm > div > div:nth-child(1) > input:nth-child(4)")
+    pw.click()
+    pw.send_keys(ent2.get())
+    time.sleep(1)
+
+    # 로그인 버튼
+    login_btn = driver.find_element(By.CSS_SELECTOR, '#ssoLoginForm > div > div.form-group.form-group-submit > input')
+    login_btn.click()
+
+    # 수강 과목들 가져오기
+    driver.get("https://www.learnus.org/?lang=")
+    learnus_page_source = driver.page_source
+    soup = BeautifulSoup(learnus_page_source, 'html.parser')
+
+    course_title_list = soup.select(
+        'div.front-box-body.course_lists > ul > li > div > a > div > div.course-title > h3 ')
+    print("\n수강 과목:")
+    for title in course_title_list:
+        print(title.text)
+
+    # 전체 알림 먼저 들어가기
+    time.sleep(1)
+    notification_btn1 = driver.find_element(By.CSS_SELECTOR,
+                                            '#page-header > div.page-util > div.usermenu > ul > li.nav-item.nav-item-userinfo > button')
+    notification_btn1.click()
+    time.sleep(1)
+
+    notification_btn2 = driver.find_element(By.CSS_SELECTOR,
+                                            '#page-userinfo > div > div.col-3.col-tab > div > a.nav-link.nav-link-noti')
+    notification_btn2.click()
+    time.sleep(1)
+
+    notification_btn3 = driver.find_element(By.CSS_SELECTOR,
+                                            '#mCSB_3_container > div > div.userinfo-title > div > div:nth-child(2) > a')
+    notification_btn3.click()
+
+    # 알림 내용들 가져오기
+
+    driver.get("https://www.learnus.org/local/ubnotification/")
+    notification_page_source = driver.page_source
+    soup = BeautifulSoup(notification_page_source, 'html.parser')
+
+    notification_list = soup.select('#page-content > div > div > div.well.wellnopadding > a > div > div')
+    print("\n공지사항 업데이트:")
+    for list in notification_list:
+        print(list.text)
+
+    while (True):
+        if keyboard.is_pressed("esc"):
+            break
+
+    driver.close()
+    print("드라이버 종료")
+
+
+
+def login(event):
+    threading.Thread(target=loggedIn()).start()
+
+
+#다음 페이지로
+def next_image():
+    global page_num
+    page_num=page_num+1
+    lab_d.configure(image=background[page_num])
+
+#두번째 페이지 버튼 생성
+def show_page2():
+    login = Button(win, text="Login", bg="#264bb4", fg="#ffffff", state=DISABLED, font=startfont,
+                   command=lambda: [next_image(), login.destroy(), show_page3()])
+    login.place(x=185, y=660)
+
     # id 라벨과 입력창
     lab1 = Label(win)
     lab1.config(text="학번")
@@ -105,20 +198,16 @@ def loggedIn():
     ent2.pack()
     ent2.place(x=63, y=390)
 
+    # 로그인 버튼 넣기
+    btn = Button(win)
+    btn.config(width=10, height=1)
+    btn.config(text="로그인")
+    btn.pack()
 
-
-
-#다음 페이지로
-def next_image():
-    global page_num
-    page_num=page_num+1
-    lab_d.configure(image=background[page_num])
-
-#두번째 페이지 버튼 생성
-def show_page2():
-    login = Button(win, text="Login", bg="#264bb4", fg="#ffffff", state=DISABLED, font=startfont,
-                   command=lambda: [next_image(), login.destroy(), show_page3()])
-    login.place(x=185, y=660)
+    # Enter 누르면 로그인
+    ent2.bind("<Return>", loggedIn())
+    # 로그인 버튼 왼쪽 클릭하면 로그인
+    btn.bind("<Button-1>", loggedIn())
 
     if loggedIn() == 1:
         login.configure(state=ACTIVE)
